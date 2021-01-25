cf0e0d299afe0aaf19856b19faaa3d268666631b
Jahaziel Israel <blaquebeardcoder@gmail.com>
2021-01-24 20:48:19 -0500

    Completed project and updated styling

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,16 +1,63 @@
-function Calculator () => {
-    return(
-        <div className="container">
-    <h1>Add with React!</h1>
-  <form className="add">
-    <input type="text" name="value1" />
-    <span>+</span>
-    <input type="text" name="value2" />
-    <span>=</span>
-    <h3>Addition results go here!</h3>
-  </form>
-</div>
-    );
+import React, { useState, useEffect, useRef } from "react";
+import './Calculator.css';
+
+function Calculator () {
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus());
+
+  function handleClick(e) {
+    if(result === "Error") {
+      setResult(e.target.name)
+    } else {
+      setResult(result.concat(e.target.name));
+    }
+  }
+
+  function backspace() {
+    setResult(result.slice(0, result.length -1))
+  }
+
+  function clear () {
+    setResult("");
+  }
+
+  function calculate () {
+    try {
+      setResult(eval(result).toString());
+    } catch(error) {
+      setResult("Error");
+    }
+  }
+
+  return(
+    <div className="calc-app">
+      <from>
+        <input type="text" value={result} ref={inputRef} />
+      </from>
+      <div className="keypad">
+        <button id="clear" onClick={clear}>Clear</button>
+        <button id="backspace" onClick={backspace}>C</button>
+        <button id="operator" name="+" onClick={handleClick}>+</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button id="operator" name="-" onClick={handleClick}>-</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button id="operator" name="*" onClick={handleClick}>&times;</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button id="operator" name="/" onClick={handleClick}>/</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button id="operator" name="." onClick={handleClick}>.</button>
+        <button id="result" onClick={calculate}>Result</button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
